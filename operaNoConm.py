8344f8051553185c0e3fc465af7e4827ff436234
campagno <campagno.eduardo@outlook.com>
2020-02-06 21:21:32 -0300

    Parte 2.1

--- operaNoConm.py
+++ operaNoConm.py
@@ -4,5 +4,27 @@ import logging
 
 logging.basicConfig(format='%(asctime)s.%(msecs)03d [%(threadName)s] - %(message)s', datefmt='%H:%M:%S', level=logging.INFO)
 
+#defino funciones y variable
+#dale un valor inicial a la variable
 
+var = 1
+
+def sumarUno():
+    global var
+    var += 1
+
+def multiplicarPorDos():
+    global var
+    var = var*2
+#ejecutá cada función en un thread separado y anotá los resultados
+t1 = threading.Thread(target=sumarUno, name='Sumar 1')
+
+
+t2 = threading.Thread(target=multiplicarPorDos, name='Multiplica por 2')
+
+t1.start()
+
+t2.start()
+
+print(f'Total vale {var}')
 
